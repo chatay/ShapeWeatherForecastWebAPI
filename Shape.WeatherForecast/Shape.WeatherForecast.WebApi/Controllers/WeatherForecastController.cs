dc22480ed349181cda379fec0cfd48b5279ca28d
chatay <cagataygr@gmail.com>
2022-05-17 21:47:44 +0300

    made a request to API to create dto based on response

--- Shape.WeatherForecast/Shape.WeatherForecast.WebApi/Controllers/WeatherForecastController.cs
+++ Shape.WeatherForecast/Shape.WeatherForecast.WebApi/Controllers/WeatherForecastController.cs
@@ -2,7 +2,10 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Shape.WeatherForecast.WebApi.Controllers
@@ -11,29 +14,31 @@ namespace Shape.WeatherForecast.WebApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly HttpClient _httpClient;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, HttpClient httpClient)
         {
             _logger = logger;
+            _httpClient = httpClient;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet(Name = "GetListOfTemperatures")]
+        public async Task<string> Get([FromQuery] string cities)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            string apiKey = "aec690a9abbd82ae0ec25b560f060c73";
+            var query = $"https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={apiKey}";
+
+            var response = await _httpClient.GetAsync(query);
+
+            if (response.IsSuccessStatusCode)
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                return "something went wrong.";
+            }
         }
     }
 }
